d9cdf7217588bd3fc69fd9ea2ba6d4ca5e066a4e
angcyo <angcyo@126.com>
2018-03-04 18:28:55 +0800

    init

--- letter/src/main/java/com/express/letter/MainActivity.kt
+++ letter/src/main/java/com/express/letter/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.express.letter
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
+import android.Manifest
+import android.content.Intent
+import com.angcyo.uiview.base.UILayoutActivity
+import com.express.letter.iview.LoginUIView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : UILayoutActivity() {
+    override fun onLoadView(intent: Intent?) {
+        checkPermissions()
+        startIView(LoginUIView())
+    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    override fun needPermissions(): Array<String> {
+        return arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_PHONE_STATE,
+                Manifest.permission.ACCESS_WIFI_STATE,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.RECORD_AUDIO,
+                Manifest.permission.CAMERA,
+                Manifest.permission.READ_CONTACTS)
     }
 }
---
+++ letter/src/main/java/com/express/letter/iview/LoginUIView.kt
@@ -0,0 +1,31 @@
+package com.express.letter.iview
+
+import com.angcyo.uiview.base.Item
+import com.angcyo.uiview.base.SingleItem
+import com.angcyo.uiview.model.TitleBarPattern
+import com.angcyo.uiview.recycler.RBaseViewHolder
+import com.express.letter.R
+import com.express.letter.base.BaseItemUIView
+
+/**
+ * Created by angcyo on 2018-03-04.
+ */
+class LoginUIView : BaseItemUIView() {
+
+    override fun getTitleBar(): TitleBarPattern {
+        return super.getTitleBar().setTitleString("登录")
+    }
+
+    override fun createItems(items: MutableList<SingleItem>) {
+        items.add(object : SingleItem() {
+            override fun onBindView(holder: RBaseViewHolder, posInData: Int, dataBean: Item?) {
+
+            }
+
+            override fun getItemLayoutId(): Int {
+                return R.layout.em_activity_login
+            }
+        })
+    }
+
+}
