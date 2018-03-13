bea5b5f2e72e20bd650844eb3cd6cf47e9dcf9c7
angcyo <angcyo@126.com>
2018-03-13 15:57:35 +0800

    fix base

--- letter/src/main/java/com/express/letter/base/BaseRecyclerUIView.kt
+++ letter/src/main/java/com/express/letter/base/BaseRecyclerUIView.kt
@@ -1,10 +1,18 @@
 package com.express.letter.base
 
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.FrameLayout
 import com.angcyo.uiview.base.UIRecyclerUIView
+import com.angcyo.uiview.widget.EmptyView
 
 /**
  * Created by angcyo on 2018/02/13 23:10
  */
 abstract class BaseRecyclerUIView<H, T, F> : UIRecyclerUIView<H, T, F>() {
-
+    override fun inflateLoadLayout(baseRootLayout: FrameLayout?, inflater: LayoutInflater?): View {
+        val emptyView: EmptyView = super.inflateLoadLayout(baseRootLayout, inflater) as EmptyView
+        emptyView.setShowType(EmptyView.SHOW_TYPE_2)
+        return emptyView
+    }
 }
