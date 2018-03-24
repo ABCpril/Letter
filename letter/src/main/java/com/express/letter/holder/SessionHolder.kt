6439348ecf5edd4c84f33bc8ec1dfb76a75194f1
angcyo <angcyo@126.com>
2018-03-24 23:09:18 +0800

    fix base

--- letter/src/main/java/com/express/letter/holder/SessionHolder.kt
+++ letter/src/main/java/com/express/letter/holder/SessionHolder.kt
@@ -1,11 +1,14 @@
 package com.express.letter.holder
 
+import android.text.TextUtils
 import com.angcyo.uiview.recycler.RBaseViewHolder
 import com.angcyo.uiview.recycler.adapter.RExItemHolder
 import com.angcyo.uiview.widget.NoReadNumView
 import com.express.letter.R
 import com.express.letter.bean.ConversationItem
 import com.express.letter.chat.ChatUIView
+import com.hyphenate.chat.EMClient
+import com.hyphenate.chat.EMConversation
 import com.hyphenate.chat.EMTextMessageBody
 
 /**
@@ -21,18 +24,36 @@ import com.hyphenate.chat.EMTextMessageBody
  */
 class SessionHolder : RExItemHolder<ConversationItem>() {
     override fun onBindItemDataView(holder: RBaseViewHolder, posInData: Int, dataBean: ConversationItem) {
-        val lastMessage = dataBean.mEMConversation.lastMessage
-        holder.tv(R.id.name_view).text = lastMessage.from
+        val conversation = dataBean.mEMConversation
+        val lastMessage = conversation.lastMessage
+
+        //显示的名字
+        var username = conversation.conversationId()
+        if (conversation.type == EMConversation.EMConversationType.GroupChat) {
+            val group = EMClient.getInstance().groupManager().getGroup(username)
+            username = if (group != null) group.groupName else username
+        } else if (conversation.type == EMConversation.EMConversationType.ChatRoom) {
+            val room = EMClient.getInstance().chatroomManager().getChatRoom(username)
+            username = if (room != null && !TextUtils.isEmpty(room.name)) room.name else username
+        } else {
+
+        }
+        holder.tv(R.id.name_view).text = username //lastMessage.from
+
+        //最后一条消息显示
         holder.tv(R.id.tip_view).text = (lastMessage.body as EMTextMessageBody).message
 
+        //消息时间显示
         holder.timeV(R.id.time_view).time = lastMessage.msgTime
 
+        //未读数显示
         val noReadNumView: NoReadNumView = holder.v(R.id.no_read_num_view)
-        val msgCount = dataBean.mEMConversation.unreadMsgCount
+        val msgCount = conversation.unreadMsgCount
         noReadNumView.noReadNum = if (msgCount == 0) -1 else msgCount
 
+        //点击跳转
         holder.clickItem {
-            startIView(ChatUIView(lastMessage.from))
+            startIView(ChatUIView(username))
         }
     }
 }
